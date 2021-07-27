fea19e865e538416cdf98768c82aa1990dc7ebe9
lathne <laurathomazi@gmail.com>
2021-07-27 09:50:38 -0300

    adicionando funçoes para mostrar/esconder senhas + ajustes nas imagens

--- src/app.js
+++ src/app.js
@@ -1,14 +1,62 @@
 // Interação animação
 
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+const signInBtn = document.querySelector('#sign-in-btn');
+const signUpBtn = document.querySelector('#sign-up-btn');
+const container = document.querySelector('.container');
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+signUpBtn.addEventListener('click', () => {
+  container.classList.add('sign-up-mode');
 });
 
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+signInBtn.addEventListener('click', () => {
+  container.classList.remove('sign-up-mode');
 });
 
+// Mostrar/Ocultar Senha
+function showHidePasswordOne() {
+  const x = document.getElementById('password1');
+  const y = document.getElementById('show1');
+  const z = document.getElementById('hide1');
+
+  if (x.type === 'password') {
+    x.type = 'text';
+    y.style.display = 'block';
+    z.style.display = 'none';
+  } else {
+    x.type = 'password';
+    y.style.display = 'none';
+    z.style.display = 'block';
+  }
+}
+
+function showHidePasswordTwo() {
+  const x = document.getElementById('password2');
+  const y = document.getElementById('show2');
+  const z = document.getElementById('hide2');
+
+  if (x.type === 'password') {
+    x.type = 'text';
+    y.style.display = 'block';
+    z.style.display = 'none';
+  } else {
+    x.type = 'password';
+    y.style.display = 'none';
+    z.style.display = 'block';
+  }
+}
+
+function showHidePasswordThree() {
+  const x = document.getElementById('password3');
+  const y = document.getElementById('show3');
+  const z = document.getElementById('hide3');
+
+  if (x.type === 'password') {
+    x.type = 'text';
+    y.style.display = 'block';
+    z.style.display = 'none';
+  } else {
+    x.type = 'password';
+    y.style.display = 'none';
+    z.style.display = 'block';
+  }
+}
