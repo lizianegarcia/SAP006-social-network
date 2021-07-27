6b7ea98ad92a49af459360ba866ae932a64fa693
lathne <laurathomazi@gmail.com>
2021-07-27 10:21:53 -0300

    ajustes js

--- src/app.js
+++ src/app.js
@@ -14,49 +14,49 @@ signInBtn.addEventListener('click', () => {
 
 // Mostrar/Ocultar Senha
 function showHidePasswordOne() {
-  const x = document.getElementById('password1');
-  const y = document.getElementById('show1');
-  const z = document.getElementById('hide1');
+  const password = document.getElementById('password1');
+  const show = document.getElementById('show1');
+  const hide = document.getElementById('hide1');
 
-  if (x.type === 'password') {
-    x.type = 'text';
-    y.style.display = 'block';
-    z.style.display = 'none';
+  if (password.type === 'password') {
+    password.type = 'text';
+    show.style.display = 'block';
+    hide.style.display = 'none';
   } else {
-    x.type = 'password';
-    y.style.display = 'none';
-    z.style.display = 'block';
+    password.type = 'password';
+    show.style.display = 'none';
+    hide.style.display = 'block';
   }
 }
 
 function showHidePasswordTwo() {
-  const x = document.getElementById('password2');
-  const y = document.getElementById('show2');
-  const z = document.getElementById('hide2');
+  const password = document.getElementById('password2');
+  const show = document.getElementById('show2');
+  const hide = document.getElementById('hide2');
 
-  if (x.type === 'password') {
-    x.type = 'text';
-    y.style.display = 'block';
-    z.style.display = 'none';
+  if (password.type === 'password') {
+    password.type = 'text';
+    show.style.display = 'block';
+    hide.style.display = 'none';
   } else {
-    x.type = 'password';
-    y.style.display = 'none';
-    z.style.display = 'block';
+    password.type = 'password';
+    show.style.display = 'none';
+    hide.style.display = 'block';
   }
 }
 
 function showHidePasswordThree() {
-  const x = document.getElementById('password3');
-  const y = document.getElementById('show3');
-  const z = document.getElementById('hide3');
+  const password = document.getElementById('password3');
+  const show = document.getElementById('show3');
+  const hide = document.getElementById('hide3');
 
-  if (x.type === 'password') {
-    x.type = 'text';
-    y.style.display = 'block';
-    z.style.display = 'none';
+  if (password.type === 'password') {
+    password.type = 'text';
+    show.style.display = 'block';
+    hide.style.display = 'none';
   } else {
-    x.type = 'password';
-    y.style.display = 'none';
-    z.style.display = 'block';
+    password.type = 'password';
+    show.style.display = 'none';
+    hide.style.display = 'block';
   }
 }
