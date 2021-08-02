80f2b429db5213402982d9f1fc61d966fc45e327
Your Name <you@example.com>
2021-08-02 14:32:19 -0300

    login google, botão sign out

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,44 +5,67 @@
 //   console.log('Olá mundo!');
 // };
 
-
-
-const logInEmail = document.getElementById('sign-in-email');
-const logInPassword = document.getElementById('password');
-const logInButton = document.getElementById('entrar');
+const logInEmail = document.getElementById('sign-in-email')
+const logInPassword = document.getElementById('password')
+const logInButton = document.getElementById('entrar')
+const logInGoogle = document.getElementById('google-login')
+const registerWithGoogle = document.getElementById('google-register')
+const logOut = document.getElementById('logout')
 
 // const signUpName = document.getElementById('sing-up-name');
-const inputEmail = document.getElementById('sign-up-email');
-const inputPassword = document.getElementById('password2');
+const inputEmail = document.getElementById('sign-up-email')
+const inputPassword = document.getElementById('password2')
 const register = document.getElementById('register')
-const auth = firebase.auth();
+const auth = firebase.auth()
 
 // Add login Event
 logInButton.addEventListener('click', () => {
   // Get email and password
-  const registeredEmail = logInEmail.value;
-  const registeredPassword = logInPassword.value;
+  const registeredEmail = logInEmail.value
+  const registeredPassword = logInPassword.value
 
-  const promise = auth.signInWithEmailAndPassword(registeredEmail, registeredPassword);
+  const promise = auth.signInWithEmailAndPassword(
+    registeredEmail,
+    registeredPassword
+  )
 
-  promise.catch((error) => console.log(error.message));
-});
+  promise.catch(error => console.log(error.message))
+})
 
 // Add Sign Up Event
 register.addEventListener('click', () => {
   // Get email and password
-  const registeredEmail = inputEmail.value;
-  const registeredPassword = inputPassword.value;
+  const registeredEmail = inputEmail.value
+  const registeredPassword = inputPassword.value
   // Create new user
-  const promise = auth.createUserWithEmailAndPassword(registeredEmail, registeredPassword);
-  promise.catch((error) => console.log(error.message));
-});
+  const promise = auth.createUserWithEmailAndPassword(
+    registeredEmail,
+    registeredPassword
+  )
+  promise.catch(error => console.log(error.message))
+})
 
 // Add a realtime listener
-auth.onAuthStateChanged((firebaseUser) => {
+auth.onAuthStateChanged(firebaseUser => {
   if (firebaseUser) {
-    console.log(firebaseUser);
+    console.log(firebaseUser)
+    logOut.classList.remove('hidden')
   } else {
-    console.log('not logged in');
+    console.log('not logged in')
+    logOut.classList.add('hidden')
   }
-});
+})
+
+registerWithGoogle.addEventListener('click', () => {
+  const provider = new firebase.auth.GoogleAuthProvider()
+  firebase.auth().signInWithPopup(provider)
+})
+
+logInGoogle.addEventListener('click', () => {
+  const provider = new firebase.auth.GoogleAuthProvider()
+  firebase.auth().signInWithPopup(provider)
+})
+
+logOut.addEventListener('click', () => {
+  firebase.auth().signOut()
+})
