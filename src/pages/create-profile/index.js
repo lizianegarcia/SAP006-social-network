a8f42be47bdc99759bcb0645ac57bb0aed59a0ea
Your Name <you@example.com>
2021-08-11 17:45:59 -0300

    fotos carregando, tudo ok

--- src/pages/create-profile/index.js
+++ src/pages/create-profile/index.js
@@ -1,47 +1,48 @@
 /* eslint-disable no-use-before-define */
+// import firebase from '../../services/firebase.js';
+
+// import firebase from "../../services/firebase";
+
 /* eslint-disable func-names */
 const createPage = () => {
-  // const user = firebase.getUser();
+  const photoURL = firebase.auth().currentUser.photoURL;
   const rootElement = document.createElement('div');
   const contentnewElement = `
     <h1>Perfil</h1>
-    <figure class='profile-area-photo-box'>
-      <img class='photo'>
-      <input type="file" id="input-file-profileImg" class='input-file-profileImg transparency' accept=".jpg, .jpeg, .png">
-    </figure>
-    <div class='name-profile-area'>
-    <h3 id='name-user'></h3>
+    <div class='profile-photo'>
+      <img id='user-photo' class='user-photo' src='${photoURL}'>
+      <input type="file" id="profileImage" class="profileImage">
+    </div> 
+    <div class='profile-name'>
+    <p id='name-user'></p>
     </div>
+    <div class='profile-interests'>
+    <i class="fas fa-book"></i>
+    <i class="fas fa-globe-americas"></i>
+    <i class="fab fa-pagelines"></i>
+    <i class="far fa-play-circle"></i>
+    <i class="fas fa-hamburger"></i>
+    <i class="far fa-star"></i>
+    <i class="fas fa-gamepad"></i>
+    <i class="fas fa-camera"></i>
+    <i class="fas fa-language"></i>
+    <i class="fas fa-running"></i>
   `;
   // registerListener
   rootElement.innerHTML = contentnewElement;
-  const inputImg = rootElement.querySelector('#input-file-profileImg');
-  const profilePic = rootElement.querySelector('.photo');
-  const userLog = rootElement.querySelector('#name-user');
-  const reF = firebase.storage().ref('profile-Images');
+  const inputImg = rootElement.querySelector('#profileImage');
+  const userPhoto = rootElement.querySelector('#user-photo');
+  const userName = rootElement.querySelector('#name-user');
 
-  const setUserData = () => {
-    firebase.auth().onAuthStateChanged((userSign) => {
-      userLog.innerHTML = userSign.displayName;
-      profilePic.src = userSign.photoURL;
-    });
-  };
-
-  const setNewProfileImg = (newPic) => {
-    profilePic.src = newPic;
-  };
-
-  const sendNewProfileImg = (changeImage) => {
-    profilePic.addEventListener('click', () => {
-      inputImg.style.opacity = 1;
-      inputImg.onchange = (event) => {
-        sendImg(event.target.files[0], changeImage);
-        inputImg.style.opacity = 0;
-      };
-    });
-  };
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user != null) {
+      userPhoto.src = user.photoURL || 'https://conteudo.imguol.com.br/blogs/174/files/2018/05/iStock-648229868-1024x909.jpg';
+      userName.innerHTML = user.displayName;
+    }
+  });
 
   const sendImg = () => {
+    const ref = firebase.storage().ref('User-images');
     inputImg.onchange = (event) => {
       const photo = event.target.files[0];
       const reader = new FileReader();
@@ -51,16 +52,17 @@ const createPage = () => {
       reader.onload = function () {
         const base64 = reader.result.split('base64,')[1];
 
-        reF
+        ref
           .child(uid)
           .putString(base64, 'base64', { contentType: 'image/png' })
           .then((snapshot) => {
             console.log('snapshot', snapshot);
-            reF
+            ref
               .child(uid)
               .getDownloadURL()
               .then((url) => {
-                console.log('string download', url);
+                console.log(url);
+                userPhoto.src = url;
                 firebase.auth().currentUser.updateProfile({
                   photoURL: url,
                 });
@@ -68,11 +70,12 @@ const createPage = () => {
           });
       };
     };
-    sendImg();
-    setUserData();
-    // changeProfileImage();
-    sendNewProfileImg(setNewProfileImg);
-    return rootElement;
   };
+
+  sendImg();
+  // firebase.updateUser();
+
+  return rootElement;
 };
+
 export default createPage;
